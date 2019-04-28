4d08b3bf0a9751c28e8ec0135ff0966d2c5574dd
jesings <jsinger@stuy.edu>
2019-04-28 16:52:15 -0400

    rectified and validated

---
+++ tester.sh
@@ -0,0 +1,14 @@
+dotpyfile="sudoku.py"
+inputfile="Sudokunew.txt"
+outputfile="outputfile"
+while read line; do
+echo $line;
+time(python $dotpyfile $inputfile $outputfile $line);
+done<<<"name,Easy-NYTimes,unsolved
+name,Medium-NYTimes,unsolved
+name,Medium-NYTimes,solved
+name,WebSudoku-Hard,unsolved
+name,WebSudoku-Evil,unsolved
+name,hardest-sudoku-telegraph,unsolved
+name,sudokugarden.de/files/100sudoku2-en.pdf-Nr-100,unsolved
+name,sudokugarden.de/files/100sudoku2-en.pdf-Nr-50,unsolved"
